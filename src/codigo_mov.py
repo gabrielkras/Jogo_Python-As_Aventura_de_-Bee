a2cc4eefd3065d761e47e6ac438bb34da1f4e014
Bruno <belotti33@gmail.com>
2016-06-28 22:35:52 -0300

    Abelha mudando de imagem.

--- src/codigo_mov.py
+++ src/codigo_mov.py
@@ -11,8 +11,8 @@ pygame.display.set_caption('Jogo Teste')
 clock = pygame.time.Clock()
 
 
-def abelhaMov(x_abelha, y_abelha):
-    tela.blit(imgAbelhaDireita, (x_abelha, y_abelha))
+def abelhaMov(mov_imagem, x_abelha, y_abelha):
+    tela.blit(mov_imagem, (x_abelha, y_abelha))
 x_abelha = (800*0.45)
 y_abelha = (600*0.8)
 
@@ -21,6 +21,7 @@ mov_y = 0
 
 imgAbelhaDireita = pygame.image.load('abelha_bee_direita.png')
 imgAbelhaEsquerda = pygame.image.load('abelha_bee_esquerda.png')
+mov_imagem = imgAbelhaDireita
 morta = False
 
 while not morta:
@@ -30,8 +31,10 @@ while not morta:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 mov_x = 5
+                mov_imagem = imgAbelhaDireita
             if event.key == pygame.K_LEFT:
                 mov_x = -5
+                mov_imagem = imgAbelhaEsquerda
             if event.key == pygame.K_DOWN:
                 mov_y = 5
             if event.key == pygame.K_UP:
@@ -46,7 +49,7 @@ while not morta:
     x_abelha += mov_x
     y_abelha += mov_y
     tela.fill(BRANCO)
-    abelhaMov(x_abelha, y_abelha)
+    abelhaMov(mov_imagem, x_abelha, y_abelha)
     pygame.display.update()
     clock.tick(60)
 pygame.quit()
